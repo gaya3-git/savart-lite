be677e540f99f582a62831c6dc86a339824b188e
Gayathri <gaya3-git>
2026-06-29 12:17:42 +0530

    Day 2: Variables and data types

---
+++ Learning/day02_variables.py
@@ -0,0 +1,15 @@
+# day02_variables.py
+# Learning variables in Python.
+monthly_income = 50000
+monthly_expenses = 32000.50
+print(monthly_income)
+print(monthly_expenses)
+user_name = "Asha"
+print(user_name)
+has_job = True
+print(has_job)
+print(type(monthly_income))
+print(type(monthly_expenses))
+print(type(user_name))
+print(type(has_job))
+print(user_name, "earns Rs.", monthly_income, "per month")
