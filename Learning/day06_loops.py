20f6b784fe9840c2cffecc8ae639ba56cdfba42a
Gayathri <gaya3-git>
2026-06-30 12:07:21 +0530

    Day 7: Loops in Python

--- Learning/day06_loops.py
+++
@@ -1,7 +0,0 @@
-# day06_loops.py
-# Learning loops.
-count = 1
-
-while count <= 5:
-    print(count)
-    count += 1
---
+++ Learning/day07_loops.py
@@ -0,0 +1,9 @@
+# day07_loops.py
+
+# Learning Python Loops.
+
+count = 2
+
+while count <= 5:
+    print(count)
+    count = count + 2
