8c493eac7e1635f97d0135684eb43ac89f3e6bf8
Gayathri <gaya3-git>
2026-06-30 11:27:04 +0530

    Day 5: Conditional statements

---
+++ Learning/day05_conditions.py
@@ -0,0 +1,14 @@
+# day05_conditions.py
+# Learning conditional statements.
+savings_percentage = 8
+
+if savings_percentage >= 30:
+    print("Excellent Savings!")
+
+elif savings_percentage >= 15:
+    print("Good Savings!")
+
+else:
+    print("Need to Improve Savings!")
+
+    print("Savings Percentage:", savings_percentage, "%")
